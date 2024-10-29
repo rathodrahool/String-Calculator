93e872fab1968f88b884180fe85634208a680095
rathodrahool <rathoodrahool@gmail.com>
2024-10-29 19:32:55 +0530

    feat: return 3 if input is 1,2

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,17 @@
 class StringCalculator {
     add(numbers) {
-        if(numbers==="") return 0;
-        if(!isNaN(numbers)) return parseInt(numbers);
+        if(numbers===""){
+            return 0;
+        } else if(!isNaN(numbers)){
+            return parseInt(numbers);
+        } else{    
+        // Handle multiple numbers
+        return numbers
+        .split(",") // Split the string by commas
+        .map(Number) // Convert each substring to a number
+        .reduce((sum, current) => sum + current, 0); // Sum all the numbers
+        }
+        
     }
 }
 module.exports = StringCalculator;
