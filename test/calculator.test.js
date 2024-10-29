d27d62ff3c499729cb8ad5a0fcdaa360bf92746a
rathodrahool <rathoodrahool@gmail.com>
2024-10-29 19:26:15 +0530

    test: return 1 if input is 1

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -10,5 +10,9 @@ describe('String Calculator', () => {
     test('should return 0 for an empty string', () => {
         expect(calculator.add("")).toBe(0);
     });
+
+    test('should return 1 for the input "1"', () => {
+        expect(calculator.add("1")).toBe(1);
+    });
     
 });
